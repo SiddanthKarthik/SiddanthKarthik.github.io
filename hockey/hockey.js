7c912c3f5dada477f8ab9c538e6449b416b44da3
SiddanthKarthik <siddanthkarthik@hotmail.com>
2021-12-20 11:35:14 -0500

    Gud

--- hockey/hockey.js
+++ hockey/hockey.js
@@ -4,9 +4,9 @@ var currentState = "";
 
 function switcheroo() {
     currentState = document.getElementById("changer").innerHTML;
-    if(currentState == "Nosotros preferimos el hockey más que el voleibol porque el voleibol es menos interesante. Los estudiantes de Edgemont les gusta ver la hockey más que ver el otro deportes porque el hockey es más divertido.") {
+    if(currentState == "Nosotros preferimos el hockey más que el voleibol porque el voleibol es menos interesante. Los estudiantes de Edgemont les gusta ver el hockey más que ver el otro deportes porque el hockey es más divertido.") {
         document.getElementById("changer").innerHTML = "Nosotros conocemos los tipos de hockey, pero Edgemont tiene el equipo de hockey sobre hielo."
     }else {
-        document.getElementById("changer").innerHTML= "Nosotros preferimos el hockey más que el voleibol porque el voleibol es menos interesante. Los estudiantes de Edgemont les gusta ver la hockey más que ver el otro deportes porque el hockey es más divertido."
+        document.getElementById("changer").innerHTML= "Nosotros preferimos el hockey más que el voleibol porque el voleibol es menos interesante. Los estudiantes de Edgemont les gusta ver el hockey más que ver el otro deportes porque el hockey es más divertido."
     }
 }
